183c73848be32e9f92c7f9dbaefb902f78431317
pranjalikawale <pranjalikawale1578@gmail.com>
2020-04-06 10:14:00 -1200

    ADDED: Feature like snake, ladder and no play

--- SnakeAndLadder.sh
+++ SnakeAndLadder.sh
@@ -1,6 +1,7 @@
 #!/bin/bash 
 
 declare -A playerPosition
+declare -A score
 playerPosition=0
 
 #display start game msg
@@ -18,5 +19,29 @@ function rollDice()
    echo $currentPositions
 }
 
+#find feature like snake, ladder & no play
+function gameFeature
+{
+	local play=$1
+	currentPosition="$(rollDice)"
+	feature=$((RANDOM%3))
+   case $feature in
+   0)
+		echo "No Play you can stays in this" ${playerPosition[$play]} "position"
+     	;;
+   1)
+		playerPosition[$play]=$((${playerPosition[$play]}+$currentPosition))
+		score[$play]="${score[$play]}  ${playerPosition[$play]}"
+		echo "Ladder found the player moves ahead on" ${playerPosition[$play]} ", $currentPosition received on the die"
+      ;;
+   2)
+		playerPosition[$play]=$((${playerPosition[$play]}-$currentPosition))
+      score[$play]="${score[$play]}  ${playerPosition[$play]}"
+      echo "Snake found the player moves behind on" ${playerPosition[$play]} ", $currentPosition received on the die"
+      ;;
+      esac
+}
+
+gamePlayer="Player1"
 start
-rollDice
+gameFeature $gamePlayer
