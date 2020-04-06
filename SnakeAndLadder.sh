e8acb52e3483122aa80420b99b1b395d997754d8
pranjalikawale <pranjalikawale1578@gmail.com>
2020-04-06 10:03:49 -1200

    ADDED: Roll a dice to find position

--- SnakeAndLadder.sh
+++ SnakeAndLadder.sh
@@ -9,4 +9,14 @@ function start()
    echo "------SNAKE AND lADDER GAME START------"
 }
 
+#function to generate random number
+function rollDice()
+{
+   UPPER_LIMIT=6
+   LOWER_LIMIT=1
+   currentPositions=$((RANDOM%($UPPER_LIMIT-$LOWER_LIMIT+1)+$LOWER_LIMIT))
+   echo $currentPositions
+}
+
 start
+rollDice
